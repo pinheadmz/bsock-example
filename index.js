121ab2c39376c082523d02d7cd09f0f2e2d56c28
Matthew Zipkin <pinheadmz@gmail.com>
2018-09-07 00:47:41 +0000

    add more calls

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const bsock = require('bsock');
-const {Network, Address, Headers} = require('bcoin');
+const {Network, Address, Headers, ChainEntry} = require('bcoin');
 const network = Network.get('regtest');
 const apiKey = 'api-key';
 
 nodeSocket = bsock.connect(network.rpcPort);
-
 
 nodeSocket.on('connect', async (e) => {
   try {
@@ -12,18 +11,27 @@ nodeSocket.on('connect', async (e) => {
     console.log('Node - Connect event:\n', e);
     console.log('Node - Attempting auth:\n', await nodeSocket.call('auth', apiKey));
     console.log('Node - Attempting watch chain:\n', await nodeSocket.call('watch chain'));
+    console.log('Node - Attempting get tip:');
+    const tip = await nodeSocket.call('get tip');
+    console.log(ChainEntry.fromRaw(tip));
 
   } catch (e) {
     console.log('Node - Connection Error:\n', e);
   } 
 });
 
+nodeSocket.bind('version', (version) => {
+  console.log('Node - Version Event -- version:\n', version);
+});
+
 nodeSocket.bind('block connect', (raw, txs) => {
-  console.log('Node - Block Connect Event -- raw:\n', Headers.fromRaw(raw));
+  console.log('Node - Block Connect Event -- raw:\n', ChainEntry.fromRaw(raw));
   console.log('Node - Block Connect Event -- txs:\n', txs); // requires bloom filter
 });
 
-
+nodeSocket.bind('chain connect', (raw, txs) => {
+  console.log('Node - Chain Connect Event -- raw:\n', ChainEntry.fromRaw(raw));
+});
 
 walletSocket = bsock.connect(network.walletPort);
 
